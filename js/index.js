20b38d31e58ae164d3319504905987f2a33bd80d
diamant1991 <diamant1988@mail.ru>
2015-02-10 14:14:04 +0200

    change select

--- js/index.js
+++ js/index.js
@@ -196,4 +196,30 @@ function changePosition(link) {
 
 
 
+// Select
+$('.slct').click(function(){
+	var dropBlock = $(this).parent().find('.drop');
 
+	if( dropBlock.is(':hidden') ) {
+		dropBlock.slideDown(200);
+
+		$(this).addClass('active');
+
+		$('.drop').find('li').click(function(){
+
+			var selectResult = $(this).html();
+
+			$(this).parent().parent().find('input').val(selectResult);
+
+			$(this).parent().parent().find('.slct').removeClass('active').html(selectResult);
+
+			dropBlock.slideUp(200);
+		});
+
+	} else {
+		$(this).removeClass('active');
+		dropBlock.slideUp(200);
+	}
+
+	return false;
+});
